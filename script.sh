b57a76ead8e9b0161a7341ca9130758bcc489c73
adu <aluta182004@gmail.com>
2024-08-17 21:34:35 +0000

    created shell

--- script.sh
+++ script.sh
@@ -11,3 +11,40 @@ sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.4
 sudo unzip sonarqube-10.4.1.88267.zip
 sudo mv sonarqube-10.4.1.88267 sonarqube
 sudo mv sonarqube /opt/
+sudo yum install nginx
+sudo cat > /etc/nginx/sites-available/jenkins.dev.kasbedlabs.com <<EOF
+upstream jenkins{
+    server 127.0.0.1:8080;
+}
+
+server{
+    listen      80;
+    server_name jenkins.dev.kasbedlabs.com;
+
+    access_log  /var/log/nginx/jenkins.access.log;
+    error_log   /var/log/nginx/jenkins.error.log;
+
+    proxy_buffers 16 64k;
+    proxy_buffer_size 128k;
+
+    location / {
+        proxy_pass  http://jenkins;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+
+        proxy_set_header    Host            $host;
+        proxy_set_header    X-Real-IP       $remote_addr;
+        proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header    X-Forwarded-Proto https;
+    }
+
+}
+EOF 
+sudo systemctl enable nginx
+sudo ln -s /etc/nginx/sites-available/jenkins.dev.kasbedlabs.com /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+sudo yum install certbot python3-certbot-nginx
+sudo certbot --nginx -d jenkins.dev.kasbedlabs.com
+sudo systemctl status certbot.timer
+
+
